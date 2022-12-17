ff3672b1f5486b9b2a5f2934d69cb4fdb999987c
Mor Mordechai Ben <mor6686@gmail.com>
2022-12-17 13:52:16 +0200

    Implementing Multi-Tier Navigation

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core//styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 
 const generateClassName = createGenerateClassName({ productionPrefix: 'ma' });
 
-function App() {
+function App({ history }) {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             {/* <Switch> */}
-            <Route path='/' component={Landing} />
             <Route exact path='/pricing' component={Pricing} />
+            <Route path='/' component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
